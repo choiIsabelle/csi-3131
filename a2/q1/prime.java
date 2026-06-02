7a3dadfae56620b06aef4ce5bbf0471d940ab31a
Kath <kcasaey411@icloud.com>
2026-06-02 15:05:38 -0400

    added q1

---
+++ a2/q1/prime.java
@@ -0,0 +1,9 @@
+/*
+the user will run the program and will enter a
+number on the command line. The program will then create a separate thread that outputs
+all the prime numbers less than or equal to the number entered by the user.
+*/
+
+public class prime {
+    
+}
